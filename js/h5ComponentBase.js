e7b5a58c7d8405afcab1860e6b79c6f605a917b4
ushowjack <441332608@qq,com>
2017-01-20 12:13:09 +0800

    添加注释
    对一些重要的地方添加一些注释，然后注明一些容易写错的代码。

--- js/h5ComponentBase.js
+++ js/h5ComponentBase.js
@@ -1,11 +1,20 @@
 /* 图文组件对象*/
+
+// 以下其实就是一个模块化的过程，将所有的变量封装在一个函数中，通过传递参数，在模块内部实现
+// 创建好一个jQuery对象，然后将对象返回到全局词法作用域里。
+
+
+// 这是基本的图文组件，具有接受参数cfg，创建对应的div块，然后根据cfg添加各种属性和类名。
 var H5ComponentBase = function (Name,cfg) {
+
+	// 判断是否传入参数cfg
 	var cfg = cfg || {};
+	// 创建对应类型的类名，并创建component的div
 	var cls ='h5_component_' + cfg.type;
 	var component = $("<div class='h5_component_" + Name + " h5_component "+cls+" '></div>");
 
 
-
+	// 对component设置样式的属性，在这里对所有的长宽都缩小一半
 	cfg.width && component.width(cfg.width/2);
 	cfg.height && component.height(cfg.height/2);
 	cfg.text  && component.text(cfg.text);
@@ -18,7 +27,7 @@ var H5ComponentBase = function (Name,cfg) {
 		});
 	}
 
-
+	// 对component绑定时间，给组件添加类
 	component.on("onLeave",function () {
 		component.addClass(cls + '_leave').removeClass(cls + '_load');
 		cfg.animateOut && component.animate(cfg.animateOut,1000);
@@ -29,6 +38,8 @@ var H5ComponentBase = function (Name,cfg) {
 		cfg.animateIn && component.animate(cfg.animateIn,1000);
 		return false;
 	}); 
+
+	// 返回组件
 	return component;
 
 }
